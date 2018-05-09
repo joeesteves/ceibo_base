2bb3e33cb6726e581a4335d273ee889126608760
José Esteves <ajesteves@gmail.com>
2018-05-09 08:10:04 -0300

    add react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
-// import React from 'react'
-alert("working")
+import React from 'react'
+import ReactDOM from "react-dom"
+
+const HelloReact = (props) => (
+  <div>HELLO REACT!</div>
+)
+
+ReactDOM.render(<HelloReact />, document.getElementById("my-react-app"))
