0f09edbec895e17cb0777c9ad03ab69caf256b9a
José Esteves <ajesteves@gmail.com>
2018-07-02 15:45:52 -0300

    Import  bootstrap from node  extracted phoenix css basic style

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,9 @@ import "phoenix_html"
 import React from 'react'
 import ReactDOM from "react-dom"
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../css/phoenix.css'
+
 const HelloReact = (props) => (
   <div>HELLO REACT!</div>
 )
